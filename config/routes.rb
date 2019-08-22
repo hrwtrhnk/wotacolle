ce53d554295c092a64131ce25811183ec2b6b833
わたり。 <yue.hinako.0175@gmail.com>
2019-08-22 13:31:02 +0900

    signup/in-view in devise-view

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,4 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :new, :edit, :update]
 
-  get "signup", to: "signup#index"
-  resources :signup do
-    collection do
-      get "registration"
-      get "login"
-    end
-  end
 end
