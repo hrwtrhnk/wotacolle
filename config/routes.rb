612751c725302ae1e3bf5ccccb9f0690fcb303a5
わたり。 <yue.hinako.0175@gmail.com>
2019-08-24 17:19:23 +0900

    items/new-markup

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   resources :users, only: [:index, :show, :new, :edit, :update]
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show, :new]
   resources :mypages, only: [:index]
 
 end
