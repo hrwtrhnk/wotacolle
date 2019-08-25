af3da10601a3bbed9e8495c154364683e008ec75
わたり。 <yue.hinako.0175@gmail.com>
2019-08-25 15:26:59 +0900

    items/new set form and upload image to S3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,24 @@ class ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to mypages_path
+    else
+      render :new
+    end
   end
   
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:id, :image, :caption)
+  end
+
 end
